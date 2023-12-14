377e7095586cce480f3b7d82dfd2b806be4ddbfa
atharhasan <athar_h12@yahoo.com>
2023-12-14 18:44:56 +0200

    commit task

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * main - main function for testing
+ */
+int main(int argc, char *argv[])
+{
+    const char *filename;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: %s file\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    filename = argv[1];
+    proc_file(filename);
+
+    return 0;
+}
