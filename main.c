b388a98e61f9b00b24c19fbac196a4e7036998c7
Mohammad <mohammadelkhiat@gmail.com>
2023-12-17 00:21:18 +0200

    0_0

--- main.c
+++ main.c
@@ -2,28 +2,32 @@
 
 /**
  * main - main function for testing
+ * @argc: number
+ * @argv: function
+ * Return: 0
  */
+
 int main(int argc, char *argv[])
 {
-    char *filename;
-    stack_t *stack = NULL;
+	char *filename;
+	stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: %s file\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: %s file\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
 
-    filename = argv[1];
-    proc_file(filename, &stack);
+	filename = argv[1];
+	proc_file(filename, &stack);
 
-    while (stack != NULL)
-    {
-        stack_t *temp = stack;
-        stack = stack->next;
-        free(temp);
-    }
+	while (stack != NULL)
+	{
+		stack_t *temp = stack;
 
-    return 0;
+		stack = stack->next;
+		free(temp);
+	}
+
+	return (0);
 }
-
