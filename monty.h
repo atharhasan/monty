87e2d9ea14563b09b2cd9467dbbeb3b829ffa9d1
Mohammad <mohammadelkhiat@gmail.com>
2023-12-17 00:57:11 +0200

    0_0

--- monty.h
+++ monty.h
@@ -53,8 +53,7 @@ void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 int isInteger(char *str);
-
-
-
+void nop(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 
 #endif
